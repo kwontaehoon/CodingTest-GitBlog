789b3e610ccbc3156f62493f6125c2441cd293f9
kwontaehoon <gju04195@gmail.com>
2023-12-11 03:28:09 +0900

    feat: update

--- _posts/Programmers/2023/test.js
+++ _posts/Programmers/2023/test.js
@@ -1,14 +1,15 @@
-function solution(d, budget) {
-    // var answer = 0;
-    // const d_sum = d.sort((a, b)=> a - b).reduce((sum, curr, index)=>{
-    //     if(sum <= budget){ answer = index; }return sum+curr;
-    // });
-    // return d_sum == budget ? d.length : d_sum < budget ? answer+1 : answer;
-
-    return d.sort((a, b) => a - b).reduce((count, price) => {
-        console.log('aa: ', count, price, budget, (budget -= price) >= 0);
-        return count + true;
-    }, 0);
+function solution(number) {
+    var answer = 0;
+    for (i = 0; i <= number.length; i++) {
+        for (j = i + 1; j < number.length; j++) {
+            console.log("aa: ", number[i], number.slice(j, j+2), number.slice(j, j+2).reduce((sum, curr)=>sum+curr));
+            if (number[i] + number.slice(j, j+2).reduce((sum, curr)=>sum+curr) == 0) {
+                console.log("gggggggggggggg: ", number[i], number.slice(j, j+2));
+                answer++;
+            }
+        }
+    }
+    console.log("answer: ", answer);
 }
 
-console.log(solution([3,5,5,6], 9));
+console.log(solution([-3, -2, -1, 0, 1, 2, 3]));
