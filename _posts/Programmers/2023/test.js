81959350715198bcd9e5061c243950f1ce25f73d
kwontaehoon <gju04195@gmail.com>
2023-12-20 06:55:36 +0900

    feat: programmers 103, 104, 105(ing) update

--- _posts/Programmers/2023/test.js
+++ _posts/Programmers/2023/test.js
@@ -1,9 +1,26 @@
-    function solution(cards1, cards2, goal) {
-        const answer = goal.map(x=>{
-            if(cards1[0] == x){ cards1.shift(); return 1; }
-            if(cards2[0] == x){ cards2.shift(); return 1; }
-        });
-        return answer.includes() ? 'No' : 'Yes'
-    }
+function solution(k, m, score) {
 
-console.log(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"]));
+    const aa = score.reduce((sum, curr)=>sum+curr);
+    console.log("aa: ", aa);
+    var answer = [];
+    const test = score.sort((a, b) => b - a).join("");
+    console.log("test: ", test);
+
+    for(i=0; i<k; i++){
+        answer.push(test.substring(i*m, i*m+m).split("").sort());
+    }
+    console.log("answer: ", answer);
+
+    const test3 = answer.filter(x=>x.length == m);
+    console.log("test3: ", test3);
+
+    const test4 = test3.reduce((sum, curr)=>{
+        console.log("sum: ", sum);
+        console.log("curr: ", curr[0]);
+        return Number(sum) + Number(curr[0])*m
+    }, 0);
+    console.log("test4: ", test4);
+    
+    
+}
+console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
