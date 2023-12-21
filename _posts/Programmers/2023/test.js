440728f5e92990819071c00b02475173e7f716a0
kwontaehoon <gju04195@gmail.com>
2023-12-21 23:55:14 +0900

    feat: programmers 105, 106, 107 update

--- _posts/Programmers/2023/test.js
+++ _posts/Programmers/2023/test.js
@@ -1,26 +1,17 @@
-function solution(k, m, score) {
-
-    const aa = score.reduce((sum, curr)=>sum+curr);
-    console.log("aa: ", aa);
-    var answer = [];
-    const test = score.sort((a, b) => b - a).join("");
-    console.log("test: ", test);
-
-    for(i=0; i<k; i++){
-        answer.push(test.substring(i*m, i*m+m).split("").sort());
+function solution(nums) {
+    var result = 0;
+    for(i=0; i<=nums.length; i++){
+        for(j=i+1; j<nums.length; j++){
+            for(k=j+1; k<nums.length; k++){
+                let sum = nums[i]+nums[j]+nums[k];
+                let count = 0;
+                for(q=1; q<=sum; q++){
+                    if(sum%q == 0)count++;
+                }
+                count == 2 ? result++ : ''
+            }
+        }
     }
-    console.log("answer: ", answer);
-
-    const test3 = answer.filter(x=>x.length == m);
-    console.log("test3: ", test3);
-
-    const test4 = test3.reduce((sum, curr)=>{
-        console.log("sum: ", sum);
-        console.log("curr: ", curr[0]);
-        return Number(sum) + Number(curr[0])*m
-    }, 0);
-    console.log("test4: ", test4);
-    
-    
+    return result;
 }
-console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));
+console.log(solution([1,2,7,6,4]));
