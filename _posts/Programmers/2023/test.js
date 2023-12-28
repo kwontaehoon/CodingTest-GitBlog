1893de4f5a2cb04581d05195430305108f9a2da5
kwontaehoon <gju04195@gmail.com>
2023-12-29 06:28:24 +0900

    feat: programmers 112, 113(ing) update

--- _posts/Programmers/2023/test.js
+++ _posts/Programmers/2023/test.js
@@ -1,10 +1,10 @@
-function solution(N, stages) {
+function solution(dartResult) {
 
-    const test = [[1, 3], [2, 1], [3, 2]];
-
-    test.sort((a,b) => b[1] - a[1]);
+    var answer = 0;
     
-    console.log("test: ", test);
+    dartResult = dartResult.split("");
+    
+    return answer;
 }
 
-console.log(solution(6, [2,1,2,4,2,4,3,3]));
+console.log(solution("1S2D*3T#"));
