4630571716f2be72780be856d54a52cdc0eaf68e
kwontaehoon <gju04195@gmail.com>
2023-12-30 23:28:03 +0900

    feat: programmers 113 114(ing) update

--- _posts/Programmers/2023/test.js
+++ _posts/Programmers/2023/test.js
@@ -1,10 +1,23 @@
-function solution(dartResult) {
+function solution(X, Y) {
+    
+    var answer = "";
+    let temp = "";
 
-    var answer = 0;
-    
-    dartResult = dartResult.split("");
-    
-    return answer;
+    X.length > Y.length ? (console.log("hi"), temp = Y, Y = X, X = temp) : "";
+    X = X.split("").sort((a, b) => b - a).join("");
+    Y = Y.split("").sort((a, b) => b - a).join("");
+
+    for(i=0; i<=X.length; i++){
+        if(Y.length == 0) break;
+        if(Y.includes(X.charAt(i))){
+            Y = Y.replace(X.charAt(i), "");
+            answer += X.charAt(i);
+        }
+    }
+
+    console.log("answer: ", answer);
+
+    return answer == "" ? "-1" : answer == 0 ? "0" : answer;
 }
 
-console.log(solution("1S2D*3T#"));
+console.log(solution("5525", "1255"));
