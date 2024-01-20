cb921de9b588fefeeb59e95defbc12a400388097
kwontaehoon <gju04195@gmail.com>
2024-01-21 00:32:09 +0900

    feat: programmers 136, 137 update

--- _posts/Programmers/2024/test.js
+++ _posts/Programmers/2024/test.js
@@ -1,6 +1,22 @@
-function solution(n, a, b) {
+function solution(k, tangerine) {
 
+    const arr = Array.from({length: tangerine.length}, () => 0);
+
+    for(i of tangerine){
+        arr[i-1] += 1;
+    }
+    arr.sort((a, b) =>b-a);
+    console.log("arr: ", arr);
+    
+    for(i=0; i<arr.length; i++){
+        console.log("i: ", i);
+        k -= arr[i];
+        console.log("k: ", k);
+        if(k <= 0){
+            return i+1;
+        }
+    }
     
 }
 
-console.log(solution(8,4,7));
+console.log(solution(3, [1, 1, 2, 2]));
