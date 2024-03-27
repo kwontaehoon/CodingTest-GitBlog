d5f40099141ef6ea048667a9bd203166326b6c2e
kwontaehoon <gju04195@gmail.com>
2024-03-27 18:55:20 +0900

    feat: twin.macro 튜토리얼 add

--- _posts/Programmers/2024/test.js
+++ _posts/Programmers/2024/test.js
@@ -1,42 +1,15 @@
-function solution(progresses, speeds) {
-
-  var answer = [];
-  const arr = [];
-  let a = [];
-
-  for(let i=0; i<progresses.length; i++){
-
-    let sum = progresses[i];
-    let count = 0;
-    while(sum < 100){
-      sum += speeds[i];
-      count++;
-    }
-
-    arr.push(count);
-    console.log("arr: ", arr);
+function solution(s){
     
+  let answer = [];
+
+  for(i of s){
+      if(i == answer[answer.length-1]){
+          answer.pop();
+      }else answer.push(i);
   }
 
-  let max = arr[0];
-  a.push(arr[0]);
-  arr.shift();
-  console.log("arr2: ", arr);
-
-  for(let i=0; i<arr.length; i++){
-    console.log("i: ", i);
-    if(max >= arr[i]){
-      a.push(arr[i]);
-    }else{
-      answer.push(a.length);
-      a = [arr[i]];
-      max = arr[i];
-    }
-    console.log("a: ", a);
-  }
-  answer.push(a.length);
-
-  return answer;
+  return answer.length == 0 ? 1 : 0;
 }
 
-console.log(solution([98,99,98], [1,1,1]));
+console.log(solution("baabaa"));
+// 50 50 70 80
