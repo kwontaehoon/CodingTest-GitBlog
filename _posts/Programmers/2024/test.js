800462f9645de62e09a5d1427d3fcdb4d774f522
kwontaehoon <gju04195@gmail.com>
2024-05-01 03:11:51 +0900

    feat: programmers 152, 153 update

--- _posts/Programmers/2024/test.js
+++ _posts/Programmers/2024/test.js
@@ -1,15 +1,31 @@
-function solution(s){
-    
-  let answer = [];
+function solution(land){
+  let answer = 0;
+  let count = null;
+  let rank = 0;
 
-  for(i of s){
-      if(i == answer[answer.length-1]){
-          answer.pop();
-      }else answer.push(i);
+  for(let i=0; i<land.length; i++){
+    console.log("i: ", i);
+
+    let max = 0;
+
+    for(let j=0; j<land[i].length; j++){
+      console.log("j: ", j, count, rank);
+      if(i == land.length-1){
+        answer += Math.max(...land[land.length-1].filter((_, index) => index !== rank));
+        return answer;
+      }
+      console.log("aa: ", land[i][j], Math.max(...land[i+1].filter((_, index)=> index !== j)));
+      if(rank !== j && land[i][j] + Math.max(...land[i+1].filter((_, index)=> index !== j)) > max){
+        max = land[i][j] + Math.max(...land[i+1].filter((_, index)=> index !== j));
+        count = j;
+      }
+      console.log("max: ", max, count);
+    }
+    answer += land[i][count];
+    rank = count;
+    console.log("result: ", answer, count);
+
   }
-
-  return answer.length == 0 ? 1 : 0;
 }
 
-console.log(solution("baabaa"));
-// 50 50 70 80
+console.log(solution([[1,2,3,5], [5,6,7,8], [4,3,2,1]]));
